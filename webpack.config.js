319665c0873db3491a7ad1a37af5e22ab6ddf8b7
Ajai <ajainarayanan@gmail.com>
2016-06-24 08:42:03 -0700

    Initial commit of redux-undoredo reducer

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+var webpack = require('webpack');
+
+module.exports = {
+  context: __dirname + '/src',
+  entry: {
+    'redux-undoredo': './redux-undoredo.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  },
+  output: {
+    filename: './[name].js',
+    path: __dirname + '/dist',
+    library: 'reduxUndoredo',
+    libraryTarget: 'umd'
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+        drop_console: false,
+        dead_code: true
+      },
+      output: {
+        comments: false
+      }
+    })
+  ]
+}
